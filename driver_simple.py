200c7394726ed71992e73868f55fa46b76adf0f3
indahcs <sanclar8c@gmail.com>
2021-11-11 16:58:36 +0700

    update heroes

--- driver_simple.py
+++ driver_simple.py
@@ -9,20 +9,18 @@ engine = knowledge_engine.engine(__file__)
    
 def bc_test_questions():
 
-    engine.reset()      # Allows us to run tests multiple times.
+    engine.reset()  
 
-    engine.activate('bc_simple_rules_questions') #STUDENTS: you will need to edit the name of your rule file here
+    engine.activate('bc_simple_rules_questions') 
 
-    print("doing proof")
+    print("search hero")
     
     try:
-        with engine.prove_goal('bc_simple_rules_questions.heroes($hero)') as gen: #STUDENTS: you will need to edit this line
+        with engine.prove_goal('bc_simple_rules_questions.heroes($hero)') as gen:
             for vars, plan in gen:
-                print("The character is: %s" % (vars['hero'])) #STUDENTS: you will need to edit this line
+                print("The character is: %s" % (vars['hero'])) 
 
     except Exception:
-        # This converts stack frames of generated python functions back to the
-        # .krb file.
         krb_traceback.print_exc()
         sys.exit(1)
 
